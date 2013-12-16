31de70f59e67ecae8177289a98797e1cf3fecde4
John Kuner <jkuner@wikia-inc.com>
2013-12-15 21:15:18 -0800

    updated animations and support for going all blank

--- arduino/hackathon_arduino.ino
+++ arduino/hackathon_arduino.ino
@@ -1,6 +1,9 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 5
+int wait_T=40; //This is the delay between moving back and forth and per pixel
+int PixelCount=8; //Set this to the AMOUNT of Led's/Pixels you have or want to use on your strip And It can be used to tell where to Stop then return the eye at in the strip
+int Pixel_Start_End=0; //Set this to where you want it to Start/End at
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
@@ -21,19 +24,38 @@ void setup() {
 }
 
 void loop() {
-  InputChar = Serial.read();
+  
+  if(Serial.available()) {
+    InputChar = Serial.read(); 
+  }
   switch (InputChar) {
+    case '0':
+     colorWipe(strip.Color(0, 0, 0), 0);
+     break;
     case '1':
       Serial.write("Red");
-      colorWipe(strip.Color(255, 0, 0), 50); // Red
+      for(int i=1;i<10;i++) {
+        //Example: CylonEyeUp(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+        CylonEyeUp(strip.Color(175,0,0), strip.Color(25,0,0), strip.Color(10,0,0), wait_T, PixelCount, Pixel_Start_End);
+        delay(wait_T);
+        //Example: CylonEyeDown(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+        CylonEyeDown(strip.Color(175,0,0), strip.Color(25,0,0), strip.Color(10,0,0), wait_T, PixelCount, Pixel_Start_End);
+        delay(wait_T);
+      }
+      for(int i=1;i<5;i++) {
+        colorWipe(strip.Color(50, 0, 0), 5);
+        delay(wait_T);
+        colorWipe(strip.Color(255, 0, 0), 5);
+        delay(wait_T);
+      } 
       break;
     case '2':
       Serial.write("Yellow");
-      colorWipe(strip.Color(255, 255, 0), 50); // Yellow
+      colorWipe(strip.Color(255, 255, 0), 250); // Yellow
       break;
     case '3':
       Serial.write("Green");
-      colorWipe(strip.Color(0, 255, 0), 50); // Green
+      colorWipe(strip.Color(0, 255, 0), 250); // Green
       break;
 
   }
@@ -89,4 +111,34 @@ uint32_t Wheel(byte WheelPos) {
    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
+
+
+void CylonEyeUp(uint32_t Co, uint32_t Ct, uint32_t Ctt, uint8_t Delay, int TotalPixels, int pStart) {
+  for(int i=pStart; i<TotalPixels; i++) {
+    strip.setPixelColor(i+1, Ct);   //Second Dot Color
+    strip.setPixelColor(i, Co);     //Center Dot Color
+    strip.setPixelColor(i-1, Ct);   //Second Dot Color
+
+      strip.setPixelColor(i-2, strip.Color(0,0,0)); //Clears the dots after the 2rd color
+    strip.show();
+    //Serial.println(i); //Used For pixel Count Debugging
+    delay(Delay);
+  }
+}
+
+void CylonEyeDown(uint32_t Co, uint32_t Ct, uint32_t Ctt, uint8_t Delay, int TotalPixels, int pEnd) {
+  for(int i=TotalPixels-1; i>pEnd; i--) {
+    strip.setPixelColor(i-1, Ct);   //Second Dot Color
+    strip.setPixelColor(i, Co);    //Center Dot Color
+    strip.setPixelColor(i+1, Ct);  //Second Dot Color
+      strip.setPixelColor(i+2, strip.Color(0,0,0)); //Clears the dots after the 2rd color
+    
+    strip.show();
+    //Serial.println(i); //Used For pixel Count Debugging
+    delay(Delay);
+  }
+}
+
+
+
 
